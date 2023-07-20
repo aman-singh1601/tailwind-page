5801ef7166cad0f4185133e4840897bad5c814ae
aman-singh1601 <aman16singh2001@gmail.com>
2023-07-20 23:20:04 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ function App() {
 
 
   return (
-    <div className='body flex flex-col md:flex-row  '>
-      <div className="nav w-[100vw] fixed bottom-1  h-[20px] flex flex-row justify-evenly items-center sm:left-0 sm:flex-col sm:top-0 sm:h-[100vh] sm:justify-between sm:pl-1 sm:w-fit">
-        <div className='invisible w-0 h-0 sm:h-[80px] sm:flex sm:flex-col sm:justify-around sm:mt-2 sm:w-fit sm:visible'>
+    <div className='body flex flex-col w-[100vw] md:flex-row  '>
+      <div className="nav w-[100vw] fixed bottom-1  h-[20px] flex flex-row justify-evenly items-center md:left-0 md:flex-col md:top-0 md:h-[100vh] md:justify-between md:pl-1 md:w-fit ">
+        <div className='invisible w-0 h-0 md:h-[80px] md:flex md:flex-col md:justify-around md:mt-2 md:w-fit md:visible'>
                 <img className='h-10' src='https://svgsilh.com/svg/2034617-3f51b5.svg'/>
                 <div className=' hover:shadow-lg duration-[250ms] rounded-sm  hover:bg-[#205cbd]'>
                 <Icon className=' text-gray-400' size='sm' icon={SearchIcon}/>
                 </div>
         </div>
-        <div className='icons w-[100vw]  bottom-1  h-[20px] flex flex-row justify-evenly items-center sm:left-0 sm:flex-col sm:h-[250px] sm:justify-between sm:w-[35px]'>
+        <div className='icons w-[100vw]  bottom-1  h-[20px] flex flex-row justify-evenly items-center md:left-0 md:flex-col md:h-[250px] md:justify-between md:w-[35px]'>
               <div className=' hover:shadow-lg duration-[250ms] rounded-sm  hover:bg-[#205cbd]'>
                   <Icon className=' text-gray-400' size='lg' icon={HomeIcon}/>
               </div>
@@ -36,24 +36,23 @@ function App() {
                   <Icon className=' text-gray-400' size='lg' icon={UserIcon}/>
                 </div>
                 
-              <div className='hover:shadow-lg duration-[250ms] rounded-sm  hover:bg[#205cbd] sm:hidden'>
+              <div className='hover:shadow-lg duration-[250ms] rounded-sm  hover:bg[#205cbd] md:hidden'>
                 <Icon className=' text-gray-400'size='lg' icon={SearchIcon}/>
               </div>
        </div>
-       <div className='invisible w-0 h-0  sm:visible  sm:h-[80px] sm:flex sm:flex-col sm:items-center sm:justify-around '> 
+       <div className='invisible w-0 h-0  md:visible  md:h-[80px] md:flex md:flex-col md:items-center md:justify-around '> 
               <div className=' hover:shadow-lg duration-[250ms] rounded-sm  hover:bg-[#205cbd] '>
                 <Icon className=' text-gray-400 ' size='sm' icon={BellIcon}/>
               </div>
-              <div className=' shadow-lg rounded-sm duration-[250ms] hover:bg-[#205cbd] '>
+              <div className=' hover:shadow-lg rounded-sm duration-[250ms] hover:bg-[#205cbd] '>
                 <Icon className=' text-gray-400' size='md' icon={LogoutIcon}/>
               </div>
        </div>
         
       </div>
-      <div className='md:h-[100vh] md:w-[200px]'></div>
-       <div className="profile  w-[90%] my-[10%]  m-auto  flex flex-col items-center  ">
+       <div className="profile  w-[90%]   m-auto  flex flex-col items-center md:w-[20%] md:ml-[50px] md:bg-[#f2f4f7] md:h-[100vh] md:m-0 md:pt-8  ">
         
-        <div className='user w-[100%] my-2  flex flex-row space-x-4'>
+        <div className='user w-[100%] my-2  flex flex-row space-x-4 md:px-2'>
             <img className='h-[60px] rounded-[50%]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7rAXspe1a2jEJ0Ml5kaRrz0SUQsyJv2vHSFbzHXxeVw&s" alt="" />
             <div className='flex flex-col'>
             <span className='text-3xl font-serif'>Hi Mike</span>
@@ -91,47 +90,89 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="stat  my-[10%] w-[90%]  m-auto  flex flex-col ">
+      <div className="stat   w-[90%]  m-auto  flex flex-col md:w-[50%] md:mt-12 ">
       <h1 className='text-[15px] px-4  font-sans font-bold text-[#205cbd]'>Retirement Income</h1>
-      <h1 className='text-[20px] px-4 pb-4 font-mono font-bold text-black'>Starting Year 2056</h1>
-      <div className='flex flex-row flex-wrap justify-between px-4'>
-          <div className='flex flex-col w-[100%] py-2 border-b border-[#205cbd]'>
+      <h1 className='text-[20px] px-4 pb-4 font-mono font-bold text-black md:pb-[10px]'>Starting Year 2056</h1>
+      <div className='flex flex-row flex-wrap justify-between px-4 '>
+          <div className='flex flex-col w-[100%] py-2 border-b border-[#205cbd] md:w-[200px] md:py-0'>
               <span className='font-bold font-mono text-2xl text-black '>$3000,000</span>
               <span className='text-[11px] font-sans font-medium text-gray-500'>My Goal</span>
           </div>
-          <div className='flex flex-col w-[40%] py-2 border-b border-[#205cbd]'>
+          <div className='flex flex-col w-[40%] py-2 border-b border-[#205cbd] md:w-[200px] md:py-0'>
               <span className='font-bold font-mono text-2xl text-black '>59%</span>
               <span className='text-[11px] font-sans font-medium text-gray-500'>Goal Achieved</span>
           </div>
-          <div className='flex flex-col w-[40%] py-2 border-b border-[#205cbd]'>
+          <div className='flex flex-col w-[40%] py-2 border-b border-[#205cbd] md:w-[200px] md:py-0'>
               <span className='font-bold font-mono text-2xl text-black '>$300</span>
               <span className='text-[11px] font-sans font-medium text-gray-500'>Est. Monthly Income</span>
           </div>
       </div>
-      <span className='text-[18px] px-4 mb-4 font-sans font-bold mt-10'>Contributions Overtime</span>
+      <span className='text-[18px] px-4 mb-4 font-sans font-bold mt-10 md:mt-8'>Contributions Overtime</span>
       <div className=' graph-protion px-4 flex flex-row justify-between z-10 pb-8'>
-              <div className='flex flex-col'>
+              <div className='flex flex-col md:flex-row md:items-center md:space-x-1'>
                     <div className='h-[10px] bg-[#104191] w-[15px] rounded-md'></div>
                     <span className='text-[11px] font-sans font-medium text-gray-500'>Employer</span>
-                    <span className='font-bold font-mono text-2xl text-black'>$73,500</span>
+                    <span className='font-bold font-mono text-2xl text-black md:text-sm'>$73,500</span>
               </div>
-              <div className='flex flex-col'>
+              <div className='flex flex-col md:flex-row md:items-center md:space-x-1'>
                     <div className='h-[10px] bg-[#205cbd] w-[15px] rounded-md'></div>
                     <span className='text-[11px] font-sans font-medium text-gray-500'>Employee</span>
-                    <span className='font-bold font-mono text-2xl text-black'>$52,500</span>
+                    <span className='font-bold font-mono text-2xl text-black md:text-sm'>$52,500</span>
               </div>
-              <div className='flex flex-col'>
+              <div className='flex flex-col md:flex-row  md:items-center md:space-x-1'>
               <div className='h-[10px] bg-[#598bde] w-[15px] rounded-md'></div>
                     <span className='text-[11px] font-sans font-medium text-gray-500'>Total Interest</span>
-                    <span className='font-bold font-mono text-2xl text-black'>$244,213</span>
+                    <span className='font-bold font-mono text-2xl text-black md:text-sm'>$244,213</span>
                 </div>
       </div>
       <div className='graph w-[100%] px-4 '>
           <Graph/>
       </div>
+      <div className='invisible md:visible md: px-4 md:w-[100%]'>
+        
+        <div className=' px-4 mb-4 flex flex-col w-[100%] justify-between'>
+            <span className='text-[18px]  font-sans font-bold '>How do I compare to my peers</span>
+            <span className='text-[11px] font-sans font-medium text-gray-500'>These numbers represent current goal achievement</span>
+        </div>
+        <div className='selector px-4 flex flex-row w-[100%] '>
+          <div className='flex flex-col'>
+          <div className='flex flex-row pb-2 border-b'>
+              <span className='text-[15px] w-[45px] font-sans font-bold pt-2'>
+                Age: 
+              </span>
+              <SelectTab type="Under 30" />
+          </div>
+          <div className='flex flex-row pb-2 border-b  '>
+              <span className='text-[15px] w-[45px] font-sans font-bold pt-2'>
+                Salary: 
+              </span>
+              <SelectTab type="K20 - k30" />
+          </div>
+          <div className='flex flex-row pb-2 border-b'>
+              <span className='text-[15px] w-[45px] font-sans font-bold pt-2'>
+                Gender: 
+              </span>
+              <SelectTab  type="Male" />
+          </div>
+          </div>
+          
+          <div className='flex my-8 flex-row space-x-2 w-[100%] items-center justify-center'>
+             <div className='w-[80px]'>
+                <CircularBar value={75}/>
+             </div>
+             <div className='w-[80px]'>
+                <CircularBar value={56}/>
+             </div>
+             <div className='w-[80px]'>
+                <CircularBar value={96}/>
+             </div>
+          </div>
+        </div>
+        </div>
 
       </div>
-      <div className="performance w-[90%] my-[10%]  m-auto  flex flex-col  ">
+      <div className="performance w-[90%]   m-auto  flex flex-col  md:w-[20%] md:mt-10  ">
+        <div className='visible md:invisible md:hidden'>
         <div className=' px-4 mb-4 flex flex-col'>
             <span className='text-[18px]  font-sans font-bold '>How do I compare to my peers</span>
             <span className='text-[11px] font-sans font-medium text-gray-500'>These numbers represent current goal achievement</span>
@@ -167,6 +208,8 @@ function App() {
              </div>
           </div>
         </div>
+        </div>
+        <div className='md:bg-[#f2f4f7] md:rounded-lg md:py-4'>
         <div className='strategy px-4 flex flex-col'>
         <span className='text-[18px]  font-sans font-bold pb-4 '>Retirement Strategy</span>
         <span className='text-[15px]  font-sans font-bold pb-2' >Employee Contribution</span>
@@ -189,7 +232,19 @@ function App() {
         </div>
         <button className='w-[90%] bg-[#205cbd] text-white font-sans font-semibold rounded-md m-auto text-xl py-2 items-center'>Update</button>
         </div>
+      
+      
+      
       </div> 
+        <div className=' hidden md:px-4 md:flex md:flex-col md:border-l md:border-[#205cbd] md:my-8'>
+            <div className='text-[18px]  font-sans '>Are you considering a</div>
+            <div className='text-[18px]  font-sans font-bold pb-[6px] '>Housing Advance</div>
+            <span className='text-[11px] font-sans font-medium text-gray-500'> Limites time reduced interest.</span>
+            <button className='text-[#205cbd] font-mono w-fit'>{'Learn More >'}</button>
+        </div>
+
+
+      </div>
       
     </div>
   )
--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -1,68 +1,77 @@
-import { Card,BarChart,Subtitle } from "@tremor/react";
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis,
+	CartesianGrid } from 'recharts';
 
-const chartData=[
-    {
-        name: "20",
-        "Employee":40,
-        "Employer":20,
-        "Total Interest": 20,
-      },
-      {
-        name: "35",
-        "Employee":50,
-        "Employer":30,
-        "Total Interest": 30,
-      },
-      {
-        name: "40",
-        "Employee":70,
-        "Employer":20,
-        "Total Interest": 60,
-      },
-      {
-        name: "45",
-        "Employee":110,
-        "Employer":30,
-        "Total Interest": 80,
-      },
-      {
-        name: "50",
-        "Employee":80,
-        "Employer":110,
-        "Total Interest": 60,
-      },
-      {
-        name: "55",
-        "Employee":60,
-        "Employer":90,
-        "Total Interest": 150,
-      },
-      {
-        name: "60",
-        "Employee":110,
-        "Employer":80,
-        "Total Interest": 120,
-      },
-];
-const dataFormatter = (number) => {
-    return "$ " + Intl.NumberFormat("us").format(number).toString();
-  };
-const Graph=()=>(
-    <Card>
-        <BarChart
-            className="mt-2 h-72"
-            data={chartData}
-            index="name"
-            categories={["Employer","Employee","Total Interest"]}
-            colors={["blue","teal","sky"]}
-            valueFormatter={dataFormatter}
-            yAxisWidth={56}
-            stack={true}
-            showLegend={false}
-            showTooltip={false}
-        />
-  </Card>
-    
-);
+const Graph = () => {
+
+	// Sample data
+	const data = [
+	      	{
+            name: "20",
+            "Employee":40,
+            "Employer":20,
+            "Total Interest": 20,
+          },
+	      	{
+            name: "25",
+            "Employee":40,
+            "Employer":20,
+            "Total Interest": 20,
+          },
+	      	{
+            name: "30",
+            "Employee":40,
+            "Employer":20,
+            "Total Interest": 20,
+          },
+          {
+            name: "35",
+            "Employee":50,
+            "Employer":30,
+            "Total Interest": 30,
+          },
+          {
+            name: "40",
+            "Employee":70,
+            "Employer":20,
+            "Total Interest": 60,
+          },
+          {
+            name: "45",
+            "Employee":110,
+            "Employer":30,
+            "Total Interest": 80,
+          },
+          {
+            name: "50",
+            "Employee":80,
+            "Employer":110,
+            "Total Interest": 60,
+          },
+          {
+            name: "55",
+            "Employee":60,
+            "Employer":90,
+            "Total Interest": 150,
+          },
+          {
+            name: "60",
+            "Employee":110,
+            "Employer":80,
+            "Total Interest": 120,
+          },
+	];
+
+	return (
+		<BarChart width={300} height={250} barSize={20} data={data} >
+			<CartesianGrid />
+			<XAxis dataKey="name" />
+			<YAxis />
+			<Bar dataKey="Employee" stackId="a" fill="#205cbd" />
+			<Bar dataKey="Employer" stackId="a" fill="#104191" />
+			<Bar dataKey="Total Interest" stackId="a" fill="#598bde" />
+		</BarChart>
+	);
+}
 
 export default Graph;
